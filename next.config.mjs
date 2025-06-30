3e897692f0492bc408abc2491d8944cef5878109
김영준 <youngjoonkim94@gmail.com>
2025-06-30 21:24:31 +0900

    그냥 처음부터 웹에서 리디렉션

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/integrations',
+        permanent: true,
+      },
+    ]
+  },  
 }
 
 export default nextConfig
